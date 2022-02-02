cb8ad573499bd7342b7036767dca05d181c7b6f5
Eric Liu <eric1932@users.noreply.github.com>
2022-02-01 18:45:26 -0600

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eo pipefail
+VENV_DIR_NAME="venv"
+
+if [[ ! -d ${VENV_DIR_NAME} ]]
+then
+  echo "Initialize Python venv"
+  pip3 -m venv ${VENV_DIR_NAME}
+fi
+
+echo "Activate venv"
+source ${VENV_DIR_NAME}/bin/activate
+pip3 install -r requirements.txt
+
+uvicorn main:app --host 0.0.0.0 --port 80
